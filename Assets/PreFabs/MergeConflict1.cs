76cb61834abb63be521c3e9f57715ac47850e650
Danilo Andrade Mendoza <daniloandradem10@email.arizona.edu>
2018-09-11 13:16:34 -0700

    Merging Conflict 1 File,

---
+++ Assets/PreFabs/MergeConflict1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MergeConflict1 : MonoBehaviour {
+    public int speed = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
